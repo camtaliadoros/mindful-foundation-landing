7bdb21282cfb94db55ad710413a8fefd3fbdb0f1
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-11-01 16:14:43 +0000

    initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Landing Page</div>;
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <div className='flex text-white h-4/5 w-screen gap-8 items-center'>
+        <div className='w-1/2 flex justify-end'>Logo</div>
+        <div className='w-1/2 flex flex-col justify-start gap-2'>
+          <h2 className='text-4xl text-mf-green font-bold '>
+            Empowering Lives
+          </h2>
+          <h2 className='text-4xl text-mf-green font-bold '>Preventing Harm</h2>
+          <h2 className='text-4xl text-mf-green font-bold'>Healing Futures</h2>
+        </div>
+      </div>
+      <div className=' flex flex-col items-center justify-start h-1/5'>
+        <h3 className='text-white font-normal text-xl'>Site being updated</h3>
+        <h3 className='text-white font-bold text-xl'>
+          Please visit{' '}
+          <Link className='underline hover:no-underline transition' href='/'>
+            Think Different
+          </Link>
+        </h3>
+      </div>
+    </div>
+  );
 }
