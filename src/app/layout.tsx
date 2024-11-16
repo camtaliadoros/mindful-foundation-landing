b23d22a9e1d81b8793f4eb0e8e17370bc2046a5c
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-11-16 08:53:08 +0000

    change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,19 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const foundersGrotesk = localFont({
+  src: [
+    {
+      path: './fonts/FoundersGrotesk-Medium.otf',
+      weight: '500',
+      style: 'medium',
+    },
+    {
+      path: './fonts/FoundersGrotesk-Regular.otf',
+      weight: '400',
+      style: 'regular',
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-chalk`}
-      >
+      <body className={`${foundersGrotesk.className}  antialiased bg-chalk`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
           height={100}
           className=' max-w-36 lg:max-w-56 h-fit m-4'
         />
-        <p className='text-chalk text-right font-bold text-xs md:text-base lg:hidden m-4'>
+        <p className='font-groteskRegular text-chalk text-right font-bold text-xs md:text-base lg:hidden m-4'>
           Site being updated.
         </p>
       </div>
       <div></div>
       <div className='bg-mf-blue w-full flex self-center h-full items-center justify-center lg:justify-end row-span-4'>
-        <div className=' lg:justify-self-end lg:mr-4 flex flex-col h-8 lg:h-12 overflow-hidden [&_h2]:text-mf-green [&_h2]:text-3xl [&_h2]:lg:text-5xl [&_h2]:h-8 [&_h2]:lg:h-12  [&_h2]:mb-12 [&_h2]:inline-block [&_h2]:text-center [&_h2]:lg:text-right [&_h2]:leading-none'>
+        <div className='[&_h2]:font-groteskMedium lg:justify-self-end lg:mr-4 flex flex-col h-8 lg:h-16 overflow-hidden [&_h2]:text-mf-green [&_h2]:h-8 [&_h2]:lg:h-16  [&_h2]:mb-8 [&_h2]:lg:mb-16 [&_h2]:inline-block [&_h2]:text-center [&_h2]:lg:text-right  items-center'>
           <h2 className='first:animate-text-animation-mobile lg:first:animate-text-animation'>
             Empowering Lives
           </h2>
@@ -26,7 +26,7 @@ export default function Home() {
           <h2>Healing Futures</h2>
         </div>
       </div>
-      <div className='[&_p]:text-mf-blue  [&_p]:text-sm [&_p]:md:text-base m-4 flex flex-col justify-center row-span-4 gap-4'>
+      <div className='[&_p]:text-mf-blue [&_p]:font-groteskRegular  [&_p]:text-sm [&_p]:md:text-base m-4 flex flex-col justify-center row-span-4 gap-4'>
         <p>
           The Mindful Foundation is committed to building a world where
           children, men, and women, feel safe, supported, and connected. We work
@@ -42,15 +42,15 @@ export default function Home() {
         </p>
       </div>
       <div className='bg-mf-blue lg:flex items-end hidden'>
-        <p className='text-chalk font-bold text-xs md:text-base m-4'>
+        <p className='text-chalk font-bold text-xs md:text-base m-4 font-groteskMedium'>
           Site being updated.
         </p>
       </div>
-      <div className='p-4 [&_h3]:text-mf-blue [&_h3]:text-sm [&_h3]:lg:text-base [&_h3]:font-bold flex flex-col justify-end'>
+      <div className='[&_h3]:font-groteskMedium p-4 [&_h3]:text-mf-blue [&_h3]:text-sm [&_h3]:lg:text-base [&_h3]:font-bold flex flex-col justify-end'>
         <h3>
           To learn more about our abuse prevention initiative,{' '}
           <Link
-            className='link   hover:font-black hover:text-mf-green transition'
+            className='link font-groteskMedium  hover:font-black hover:text-mf-green transition'
             href='/'
           >
             visit Think Different.
@@ -59,7 +59,7 @@ export default function Home() {
         <h3>
           For information on how we support and protect victims of abuse,{' '}
           <Link
-            className='link hover:text-mf-green hover:font-black transition'
+            className='font-groteskMedium link hover:text-mf-green hover:font-black transition'
             href='/'
           >
             visit ListenApp.
